00176aba34c9fab5dad717888538a944178355b3
Ilia Lesik <ilialesik@gmail.com>
2017-06-16 13:30:15 +0300

    MainPage

---
+++ client/src/components/main-page/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default class MainPage extends React.PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className="">
+            <h1>ToDo</h1>
+        </div>
+    }
+}
